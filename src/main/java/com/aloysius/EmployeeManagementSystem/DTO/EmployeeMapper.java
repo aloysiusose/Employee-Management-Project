bdd4d92c90208c97a646bfb3d2af44372c36a8bc
aloysiusose <oseghalealoysius@gmail.com>
2023-05-12 10:31:54 +0100

    Changes made to the EmployeeMapper, Repository and Service classes

--- src/main/java/com/aloysius/EmployeeManagementSystem/DTO/EmployeeMapper.java
+++ src/main/java/com/aloysius/EmployeeManagementSystem/DTO/EmployeeMapper.java
@@ -3,7 +3,7 @@ package com.aloysius.EmployeeManagementSystem.DTO;
 import com.aloysius.EmployeeManagementSystem.Model.Employee;
 import com.aloysius.EmployeeManagementSystem.Service.EmployeeManagementService;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,36 +12,41 @@ import java.util.stream.Collectors;
 @Service
 @Getter
 @Setter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class EmployeeMapper {
-    Employee employee = new Employee();
-    private EmployeeDTO employeeDTO = new EmployeeDTO();
+
     @Autowired
     private EmployeeManagementService employeeManagementService;
 
-    public Employee addEmployee(Employee employee) {
-        return employeeManagementService.addEmployee(employee);
-    }
-    public List<EmployeeDTO> getAllEmployee() {
+    private static EmployeeDTO employeeByIdMapper(Employee employeeById) {
+        Employee employee = new Employee();
+        EmployeeDTO employeeDTO = new EmployeeDTO();
 
-        return employeeManagementService.listOfAllEmployee().stream().
-                map(employee -> new EmployeeDTO(employee.getId(), employee.getFirstName(),
-                        employee.getLastName(), employee.getStartDate(),
-                        employee.getDepartment()
-                )).collect(Collectors.toList());
-    }
-    private static void employeeByIdMapper(Employee employeeById, EmployeeDTO employeeDTO) {
         employeeDTO.setId(employeeById.getId());
         employeeDTO.setFirstName(employeeById.getFirstName());
         employeeDTO.setLastName(employeeById.getLastName());
         employeeDTO.setStartDate(employeeById.getStartDate());
         employeeDTO.setDepartment(employeeById.getDepartment());
+
+        return employeeDTO;
+    }
+
+    public Employee addEmployee(Employee employee) {
+
+        return employeeManagementService.addEmployee(employee);
+    }
+    public List<EmployeeDTO> getAllEmployee() {
+
+        return employeeManagementService.listOfAllEmployee().stream()
+                .map(EmployeeMapper::employeeByIdMapper)
+                .collect(Collectors.toList());
     }
     public EmployeeDTO findEmployeeById(Integer id) throws Exception {
         Employee employeeById = employeeManagementService.findEmployeeById(id);
 
+        EmployeeDTO employeeDTO = null;
         if (!employeeById.equals(null)) {
-            employeeByIdMapper(employeeById, employeeDTO);
+            employeeDTO = employeeByIdMapper(employeeById);
         }
         return employeeDTO;
 
--- src/main/java/com/aloysius/EmployeeManagementSystem/Repository/EmployeeRepository.java
+++ src/main/java/com/aloysius/EmployeeManagementSystem/Repository/EmployeeRepository.java
@@ -2,9 +2,7 @@ package com.aloysius.EmployeeManagementSystem.Repository;
 
 import com.aloysius.EmployeeManagementSystem.Model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
